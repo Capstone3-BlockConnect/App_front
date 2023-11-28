9803745ab49de3724ddde58e6b60f44cd5eb678a
Ethan Lim <studentlimsoo@gmail.com>
2023-11-29 05:02:21 +0900

    feat : 프로필 메뉴 화면 구현

--- src/screens/profile/ProfileScreen.jsx
+++ src/screens/profile/ProfileScreen.jsx
@@ -1,11 +1,34 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
-import { Text, View } from "react-native";
-
+import { Text, View, Keyboard } from "react-native";
+import styled, { css } from "styled-components";
+import * as styles from "./ProfileScreen.style";
+import CustomButton from "../../components/buttons/CustomButton";
 const ProfileScreen = () => {
   return (
-    <View>
-      <Text>Profile Screen</Text>
-    </View>
+    <styles.ContainerView>
+      <styles.WalletBox>
+        <styles.TitleWrapper>
+          <styles.WalletTitle>내 지갑</styles.WalletTitle>
+          <styles.WalletTitle>아이콘</styles.WalletTitle>
+        </styles.TitleWrapper>
+        <styles.WalletBalance>1320 Point</styles.WalletBalance>
+        <styles.WalletBalanceLabel>
+          내 활동보상 포인트
+        </styles.WalletBalanceLabel>
+      </styles.WalletBox>
+      <styles.WalletMenu>
+        <styles.WalletItem>
+          <styles.WalletItemLabel>내 프로필</styles.WalletItemLabel>
+        </styles.WalletItem>
+        <styles.WalletItem>
+          <styles.WalletItemLabel>매칭기록</styles.WalletItemLabel>
+        </styles.WalletItem>
+        <styles.WalletItem>
+          <styles.WalletItemLabel>버전</styles.WalletItemLabel>
+        </styles.WalletItem>
+      </styles.WalletMenu>
+    </styles.ContainerView>
   );
 };
 
