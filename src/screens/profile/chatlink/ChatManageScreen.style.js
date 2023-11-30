45c0819ed4f1fe1602080ee95bb1af4e27a736f9
Ethan Lim <studentlimsoo@gmail.com>
2023-11-30 21:16:45 +0900

    feat : 내 채팅링크 관리하기 화면 구현

---
+++ src/screens/profile/chatlink/ChatManageScreen.style.js
@@ -0,0 +1,140 @@
+import styled from "@emotion/native";
+
+export const Container = styled.View`
+  flex: 1;
+  flex-flow: column nowrap;
+
+  background-color: white;
+`;
+
+export const LayerContainer = styled.View`
+  flex: 1;
+  flex-flow: column nowrap;
+  width: 93%;
+  align-self: center;
+
+  row-gap: 10px;
+  background-color: white;
+`;
+
+// 환영인사
+
+export const GreetBox = styled.View`
+  flex-flow: column nowrap;
+  row-gap: 10px;
+
+  padding: 10px;
+`;
+
+export const GreetFirstArea = styled.View`
+  flex-flow: row nowrap;
+  align-items: center;
+`;
+
+export const GreetBoldText = styled.Text`
+  font-size: 17px;
+  font-weight: 900;
+`;
+
+export const GreetNormalText = styled.Text`
+  font-size: 17px;
+  font-weight: 400;
+`;
+
+// link 카드
+
+// TODO:
+// backdrop filter 적용
+export const LinkCard = styled.View`
+  border: 1px solid black;
+  padding: 12px 20px;
+
+  border-radius: 10px;
+
+  row-gap: 20px;
+`;
+
+export const WordBox = styled.View`
+  flex-flow: column nowrap;
+  row-gap: 10px;
+`;
+
+export const WordBoxTitle = styled.Text`
+  font-size: 12px;
+  color: #727272;
+  font-weight: 700;
+`;
+
+export const WordLinkText = styled.Text`
+  font-size: 15px;
+  font-weight: 400;
+`;
+
+// 버튼 영역
+
+export const ButtonBox = styled.View`
+  flex-flow: row nowrap;
+  width: 100%;
+
+  column-gap: 10px;
+
+  height: 150px;
+
+  margin-top: 20px;
+`;
+export const LeftBigArea = styled.View`
+  height: 100%;
+  width: 49%;
+`;
+
+export const RightBigArea = styled.View`
+  flex-flow: column nowrap;
+  align-items: stretch;
+
+  height: 100%;
+  width: 49%;
+
+  justify-content: space-between;
+`;
+
+export const LeftButton = styled.View`
+  background-color: #bae8e8;
+
+  width: 100%;
+  height: 100%;
+
+  align-items: center;
+  justify-content: center;
+
+  border: 0.5px solid black;
+  border-radius: 10px;
+`;
+
+export const RightUpperButton = styled.View`
+  width: 100%;
+  height: 45%;
+
+  background-color: #e5f0db;
+  align-items: center;
+  justify-content: center;
+
+  border: 0.5px solid black;
+
+  border-radius: 10px;
+`;
+
+export const RightLowerButton = styled.View`
+  width: 100%;
+  height: 45%;
+  background-color: #f4e6e7;
+  align-items: center;
+  justify-content: center;
+
+  border: 0.5px solid black;
+  border-radius: 10px;
+`;
+
+export const ButtonText = styled.Text`
+  font-size: 15px;
+  font-weight: 900;
+`;
