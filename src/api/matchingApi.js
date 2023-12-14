c7bd1b26bf4defa51d0ed91bec3865abaf3bd41a
Ethan Lim <studentlimsoo@gmail.com>
2023-12-14 14:18:41 +0900

    feat : 매칭 관련 api 추가

--- src/api/matchingApi.js
+++ src/api/matchingApi.js
@@ -40,3 +40,40 @@ export const getMatchCount = async () => {
   } finally {
   }
 };
+
+/*
+매칭 신청 로그 가져옴
+*/
+export const getMatchLog = async () => {
+  try {
+    const url = `/matchings/request/my`;
+
+    const response = await apiBase.get(url);
+
+    return { status: response.status, data: response.data };
+  } catch (e) {
+    console.error(e);
+    return e?.response;
+  } finally {
+  }
+};
+
+/*
+유저의 아이디로 닉네임 조회하기
+*/
+
+/*
+매칭 성사 로그 가져오기
+*/
+export const getMatchedLog = async () => {
+  try {
+    const url = `matchings/my`;
+    const response = await apiBase.get(url);
+
+    return { status: response.status, data: response.data };
+  } catch (e) {
+    console.error(e);
+    return e?.response;
+  } finally {
+  }
+};
